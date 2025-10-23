d492f4846762aa4a6c520faf9249e845a6161210
tuanlq236008 <lequoctuan171105@gmail.com>
2025-10-23 21:41:10 +0700

    a

--- Lab03/Cart.java
+++ Lab03/Cart.java
@@ -45,7 +45,26 @@ public class Cart {
 		}
 		return sumCost;
 	}
+	public void searchByLength(int length) { 
+		for(int i=0;i<qtyOrdered;i++) {
+			if(itemsOrdered[i].getLength()==length) {
+				System.out.printf("Found DVD: %-30s %-10.3f\n",itemsOrdered[i].getTitle(),itemsOrdered[i].getCost());
+				return;
+			}
+		}
+		System.out.println("Cannot find the DVD with id: " + length);
+	}
 	
+	public void searchByTitle(String title) { 
+		for(int i=0;i<qtyOrdered;i++) {
+			if(itemsOrdered[i].getTitle().equals(title)) {
+				System.out.printf("Found DVD: %-30s %-10.3f\n",itemsOrdered[i].getTitle(),itemsOrdered[i].getCost());
+				return;
+			}
+		}
+		System.out.println("Cannot find the DVD with title: " + title);
+	}
+
 	public void display() {
 		for(int i=0;i<qtyOrdered;i++) {
 			System.out.printf("%-10d %-30s %-10.3f\n",i+1,itemsOrdered[i].getTitle(),itemsOrdered[i].getCost());
