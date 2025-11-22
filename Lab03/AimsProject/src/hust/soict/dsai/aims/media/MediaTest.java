c6395d5a10a56436d4750b48ebb87400046e81f9
tuanlq236008 <lequoctuan171105@gmail.com>
2025-11-22 23:22:23 +0700

    Update MediaTest.java

--- Lab03/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
@@ -8,8 +8,11 @@ public class MediaTest {
 
         mediaList.add(new Book(1, "Harry Potter", "Fantasy", 50.5f));
         mediaList.add(new DigitalVideoDisc(2, "Inception", "Sci-Fi", "Christopher Nolan", 148, 20.0f));
+<<<<<<< Updated upstream:Lab03/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
         mediaList.add(new CompactDisc(3, "Thriller", "Pop", "Michael Jackson", "9", 15.0f));
     
+=======
+>>>>>>> Stashed changes:Lab04/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
         for (Media m : mediaList) {
             System.out.println(m.toString());
         }
