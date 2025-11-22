e0bebaf6d2aefa1e3bdba77bdd13ec08fc3241d9
tuanlq236008 <lequoctuan171105@gmail.com>
2025-11-22 23:21:55 +0700

    lab4

--- Lab03/AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,11 +1,10 @@
 package hust.soict.dsai.aims;
-import java.util.ArrayList;
-
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Book;
 import hust.soict.dsai.aims.media.CompactDisc;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 import hust.soict.dsai.aims.media.Media;
+import java.util.ArrayList;
 
 public class Aims {
     public static void main(String[] args) {
@@ -14,7 +13,7 @@ public class Aims {
 
         mediaList.add(new Book(1, "Harry Potter", "Fantasy", 50.5f));
         mediaList.add(new DigitalVideoDisc(2, "Inception", "Sci-Fi", "Christopher Nolan", 148, 20.0f));
-        mediaList.add(new CompactDisc(3, "Thriller", "Pop", "Michael Jackson", 9, 15.0f));
+        mediaList.add(new CompactDisc(3, "Thriller", "Pop", "Michael Jackson", "9", 15.0f));
 
         
         
--- Lab03/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/media/MediaTest.java
@@ -8,7 +8,7 @@ public class MediaTest {
 
         mediaList.add(new Book(1, "Harry Potter", "Fantasy", 50.5f));
         mediaList.add(new DigitalVideoDisc(2, "Inception", "Sci-Fi", "Christopher Nolan", 148, 20.0f));
-        mediaList.add(new CompactDisc(3, "Thriller", "Pop", "Michael Jackson", 9, 15.0f));
+        mediaList.add(new CompactDisc(3, "Thriller", "Pop", "Michael Jackson", "9", 15.0f));
     
         for (Media m : mediaList) {
             System.out.println(m.toString());
