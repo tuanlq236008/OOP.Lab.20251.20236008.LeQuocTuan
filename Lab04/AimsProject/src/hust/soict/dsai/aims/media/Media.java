0c37657d9751d5653d6dee1360aacf87a81dfdda
tuanlq236008 <lequoctuan171105@gmail.com>
2025-11-27 22:39:46 +0700

    lab4

--- Lab04/AimsProject/src/hust/soict/dsai/aims/media/Media.java
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -1,42 +1,52 @@
 package hust.soict.dsai.aims.media;
+
 import java.util.Comparator;
+
 public class Media implements Comparable<Media> {
     private int id;
     private String title;
     private String category;
     private float cost;
 
-    public Media( String title) {
-		this.title = title;
-       
-	}
-    public Media( String title, String category, float cost) {
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-      
-	}
+    public Media(String title) {
+        this.title = title;
+
+    }
+
+    public Media(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+
+    }
 
     public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
     public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 
-    
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getCategory() {
         return category;
     }
+
     public float getCost() {
         return cost;
     }
+
     public int getId() {
         return id;
     }
-    
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Media)) return false;
+        if (!(o instanceof Media))
+            return false;
         Media other = (Media) o;
         return this.title.equals(other.getTitle());
     }
@@ -47,6 +57,7 @@ public class Media implements Comparable<Media> {
                 " - Category: " + this.getCategory() +
                 " - Cost: " + this.getCost() + "$";
     }
+
     @Override
     public int compareTo(Media other) {
         return this.title.compareTo(other.getTitle());
--- Lab04/AimsProject/src/hust/soict/dsai/aims/media/Track.java
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -3,17 +3,20 @@ package hust.soict.dsai.aims.media;
 public class Track implements Playable {
     private String title;
     private int length;
+
     public Track(String title, int length) {
         this.title = title;
         this.length = length;
     }
+
     public String getTitle() {
         return title;
     }
+
     public int getLength() {
         return length;
     }
-    
+
     @Override
     public void play() {
         if (length <= 0) {
