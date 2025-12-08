46913c7aa690cbf6570cb6fa21ce11d1a2e6fed7
tuanlq236008 <lequoctuan171105@gmail.com>
2025-12-08 16:23:46 +0700

    a

---
+++ lab5/GUI/src/hust/soict/globalict/javafx/Painter.java
@@ -0,0 +1,26 @@
+package hust.soict.globalict.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Painter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Painter1.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
